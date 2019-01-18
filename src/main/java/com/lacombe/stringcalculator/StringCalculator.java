56a495a54b18c7108fc1460a7150cc3de6553d9d
Yann Savanchomkeo <yann.savanchomkeo@gmail.com>
2019-01-18 16:35:34 +0100

    Case with carriage return

--- src/main/java/com/lacombe/stringcalculator/StringCalculator.java
+++ src/main/java/com/lacombe/stringcalculator/StringCalculator.java
@@ -4,12 +4,12 @@ public class StringCalculator {
     public static int add(String stringToCalculate) {
         if (stringToCalculate.equals(""))
             return 0;
-        String[] split = stringToCalculate.split(",");
+        String separators = "\\,|\n";
+        String[] split = stringToCalculate.split(separators);
         int sum = 0;
         for (String number : split) {
             sum += Integer.parseInt(number);
         }
-
         return sum;
     }
 }
