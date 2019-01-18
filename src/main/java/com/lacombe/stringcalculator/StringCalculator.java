a2d6bc53dd74ade7b8a9ead1abb51a509edddaa9
massbath <clement.vassant@gmail.com>
2019-01-18 15:00:15 +0100

    Case with one number

--- src/main/java/com/lacombe/stringcalculator/StringCalculator.java
+++ src/main/java/com/lacombe/stringcalculator/StringCalculator.java
@@ -2,6 +2,8 @@ package com.lacombe.stringcalculator;
 
 public class StringCalculator {
     public static int add(String stringToCalculate) {
-     return 0;
+        if (stringToCalculate.equals(""))
+            return 0;
+        return Integer.parseInt(stringToCalculate);
     }
 }
--- src/test/java/com/lacombe/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/lacombe/stringcalculator/StringCalculatorTest.java
@@ -10,4 +10,14 @@ public class StringCalculatorTest {
         int stringCalculatorResult = StringCalculator.add("");
         Assert.assertEquals(0,stringCalculatorResult);
     }
+
+    @Test
+    public void withOneNumberShouldReturnNumber() {
+        int stringCalculatorResult = StringCalculator.add("1");
+        Assert.assertEquals(1, stringCalculatorResult);
+        stringCalculatorResult = StringCalculator.add("2");
+        Assert.assertEquals(2, stringCalculatorResult);
+        stringCalculatorResult = StringCalculator.add("3");
+        Assert.assertEquals(3, stringCalculatorResult);
+    }
 }
